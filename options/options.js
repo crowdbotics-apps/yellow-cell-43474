0ee4c5abdced747338194880879d5aae8019654b
Crowdbotics <team@crowdbotics.com>
2023-09-20 12:28:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-cell-43474",
+  url: "https://yellow-cell-43474.botics.co",
+  api: "https://yellow-cell-43474.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
